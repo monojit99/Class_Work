188e05313bc18ceb00cf6a08e07ae31d56b245fa
MONOJIT CHOWDHURY <55353767+monojitchowdhury@users.noreply.github.com>
2022-12-02 11:42:57 +0530

    Add files via upload

---
+++ eo.java
@@ -0,0 +1,15 @@
+public class eo
+{
+public static void main(String args[])
+{
+int a=6;
+if(a%2==0)
+{
+System.out.println(a+" "+"even number");
+}
+else
+{
+System.out.println(a+" "+"odd number");
+}
+}
+}
